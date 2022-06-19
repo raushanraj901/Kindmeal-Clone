da9790250a5cb2089e096b5fbfd6aa98cb56cdb8
Shridhar1998 <shridharhakke1998@gmail.com>
2022-06-19 13:57:59 +0530

    navbar added

--- Article/article.js
+++ Article/article.js
@@ -1,4 +1,9 @@
-
+// navbar
+import { navbar , categoryItems} from "../navbar.js";
+let n = document.getElementById("navbar");
+// let cItems = document.getElementById("items_category");
+// cItems.innerHTML = categoryItems();
+n.innerHTML = navbar();
 
 // footer
 import{footer} from "../footer/footercomponents/footer.js"
